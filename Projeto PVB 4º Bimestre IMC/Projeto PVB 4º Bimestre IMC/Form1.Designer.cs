2f65daea7dab8b6e70746a08da6674af5342a08a
Lukas Macacari de Matos <123422302+LukasMatoss@users.noreply.github.com>
2024-10-31 22:45:17 -0300

    primeira versão

---
+++ Projeto PVB 4º Bimestre IMC/Projeto PVB 4º Bimestre IMC/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Projeto_PVB_4º_Bimestre_IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            textBox1 = new System.Windows.Forms.TextBox();
+            textBox2 = new System.Windows.Forms.TextBox();
+            textBox3 = new System.Windows.Forms.TextBox();
+            textBox4 = new System.Windows.Forms.TextBox();
+            textBox5 = new System.Windows.Forms.TextBox();
+            listBox1 = new System.Windows.Forms.ListBox();
+            button6 = new System.Windows.Forms.Button();
+            label8 = new System.Windows.Forms.Label();
+            textBox6 = new System.Windows.Forms.TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            label1.Location = new System.Drawing.Point(525, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(427, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Consultório de fisioterapia";
+            label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label2.Location = new System.Drawing.Point(64, 106);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(70, 24);
+            label2.TabIndex = 1;
+            label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label3.Location = new System.Drawing.Point(64, 146);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(106, 24);
+            label3.TabIndex = 2;
+            label3.Text = "Peso (kg):";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label4.Location = new System.Drawing.Point(64, 195);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(105, 24);
+            label4.TabIndex = 3;
+            label4.Text = "Altura (m):";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label5.Location = new System.Drawing.Point(70, 386);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(152, 24);
+            label5.TabIndex = 4;
+            label5.Text = "IMC Calculado:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label6.Location = new System.Drawing.Point(70, 438);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(143, 24);
+            label6.TabIndex = 5;
+            label6.Text = "Classificação:";
+            // 
+            // button1
+            // 
+            button1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(64, 238);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(165, 34);
+            button1.TabIndex = 6;
+            button1.Text = "Calcular IMC";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button2.Location = new System.Drawing.Point(945, 243);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(112, 32);
+            button2.TabIndex = 7;
+            button2.Text = "Excluir";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button3.Location = new System.Drawing.Point(945, 202);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(112, 32);
+            button3.TabIndex = 8;
+            button3.Text = "Alterar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button4.Location = new System.Drawing.Point(945, 159);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(112, 32);
+            button4.TabIndex = 9;
+            button4.Text = "Consultar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button5.Location = new System.Drawing.Point(945, 116);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(112, 32);
+            button5.TabIndex = 10;
+            button5.Text = "Cadastrar";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox1.Location = new System.Drawing.Point(175, 103);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(320, 32);
+            textBox1.TabIndex = 11;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox2.Location = new System.Drawing.Point(175, 146);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(100, 32);
+            textBox2.TabIndex = 12;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox3.Location = new System.Drawing.Point(175, 192);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(100, 32);
+            textBox3.TabIndex = 13;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox4.Location = new System.Drawing.Point(228, 383);
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new System.Drawing.Size(150, 32);
+            textBox4.TabIndex = 14;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox5.Location = new System.Drawing.Point(228, 438);
+            textBox5.Name = "textBox5";
+            textBox5.ReadOnly = true;
+            textBox5.Size = new System.Drawing.Size(150, 32);
+            textBox5.TabIndex = 15;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // listBox1
+            // 
+            listBox1.AccessibleName = "";
+            listBox1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 24;
+            listBox1.Location = new System.Drawing.Point(805, 366);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new System.Drawing.Size(120, 76);
+            listBox1.TabIndex = 16;
+            // 
+            // button6
+            // 
+            button6.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button6.Location = new System.Drawing.Point(805, 477);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(170, 63);
+            button6.TabIndex = 17;
+            button6.Text = "Gerar Relatório HTML";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label8.Location = new System.Drawing.Point(70, 343);
+            label8.Name = "label8";
+            label8.Size = new System.Drawing.Size(99, 24);
+            label8.TabIndex = 19;
+            label8.Text = "Paciente:";
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBox6.Location = new System.Drawing.Point(175, 340);
+            textBox6.Name = "textBox6";
+            textBox6.ReadOnly = true;
+            textBox6.Size = new System.Drawing.Size(326, 32);
+            textBox6.TabIndex = 20;
+            textBox6.TextChanged += textBox6_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1442, 793);
+            Controls.Add(textBox6);
+            Controls.Add(label8);
+            Controls.Add(button6);
+            Controls.Add(listBox1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
---
+++ Projeto PVB 4º Bimestre IMC/Projeto PVB 4º Bimestre IMC/Form1.cs
@@ -0,0 +1,97 @@
+﻿/* *******************************************************************
+* Colegio Técnico Antônio Teixeira Fernandes (Univap)
+* Curso Técnico em Informática - Data de Entrega: 04/11/2024
+* Autores do Projeto: Lukas Macacari de Matos
+*                     Lucas Aguilar da Silva Nascimento
+* Turma: 2F
+* Projeto do 4º Bimestre
+* Observação: <colocar se houver>
+* 
+* problema_aula.cs
+* ************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO; //para gravação
+
+namespace Projeto_PVB_4º_Bimestre_IMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nome = textBox1.Text;
+            textBox6.Text = nome;
+
+            double peso = float.Parse(textBox2.Text);
+            double altura = float.Parse(textBox3.Text);
+            double IMC = peso / Math.Pow(altura, 2); //calculo do IMC
+            textBox4.Text = IMC.ToString("00.00"); //impressão do imc formatado com 2 casas decimais
+
+            if (IMC < 18.5) //Teste de condição para saber a classificação do nosso individuo
+                textBox5.Text = "Magreza";
+            else if (IMC >= 18.5 && IMC <= 24.9)
+                textBox5.Text = "Saudável";
+            else if (IMC >= 25.0 && IMC <= 29.9)
+                textBox5.Text = "Sobrepeso";
+            else if (IMC >= 30.0 && IMC <= 34.9)
+                textBox5.Text = "Obesidade Grau I";
+            else if (IMC >= 35.0 && IMC <= 39.9)
+                textBox5.Text = "Obesidade Grau II";
+            else
+                textBox5.Text = "Obesidade Grau III";
+
+        }
+
+        
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string text = "Olá Testando";
+            File.WriteAllText(@"C:\teste json", text);
+        }
+    }
+}
